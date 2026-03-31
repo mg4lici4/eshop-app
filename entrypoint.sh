48a25f5b45743d9007e6071d8e016921809a5b66
Galicia Negrete Macario <timgalicia@porunpaismejor.com.mx>
2026-03-31 17:11:30 -0600

    Cambio de remplazamiento de variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,12 @@
 echo "🔧 Iniciando sustitución de variables en archivos JS y HTML..."
 
 for file in /usr/share/nginx/html/*.js /usr/share/nginx/html/*.html; do
-  echo "Procesando $file..."
-  envsubst < "$file" > "$file.tmp"
-  mv "$file.tmp" "$file"
+  sed -i "s|__LOGIN__|$LOGIN|g" "$file"
+  sed -i "s|__PERSONA_REGISTRO__|$PERSONA_REGISTRO|g" "$file"
+  sed -i "s|__USUARIO_REGISTRAR__|$USUARIO_REGISTRAR|g" "$file"
+  sed -i "s|__USUARIO_SEGUNDOFA_GENERAR__|$USUARIO_SEGUNDOFA_GENERAR|g" "$file"
+  sed -i "s|__USUARIO_SEGUNDOFA_ACTIVAR__|$USUARIO_SEGUNDOFA_ACTIVAR|g" "$file"
+  sed -i "s|__FEATURE_FLAG__|$FEATURE_FLAG|g" "$file"
 done
 
 echo "✅ Sustitución terminada. Arrancando Nginx..."
