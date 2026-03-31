6fb5cb559f4a3c7c0ea89ddf2a206c75124bd773
Galicia Negrete Macario <timgalicia@porunpaismejor.com.mx>
2026-03-31 15:32:16 -0600

    Corrección sustitucion de variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 # Sustituir variables en todos los archivos JS generados por Angular
 for file in /usr/share/nginx/html/*.js; do
-  envsubst < "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+  envsubst '$LOGIN $PERSONA_REGISTRO $USUARIO_REGISTRAR $USUARIO_SEGUNDOFA_GENERAR $USUARIO_SEGUNDOFA_ACTIVAR $FEATURE_FLAG' \
+    < "$file" > "$file.tmp" && mv "$file.tmp" "$file"
 done
 
 exec nginx -g 'daemon off;'
