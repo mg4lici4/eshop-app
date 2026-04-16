25f454f3d3e77946960e2a165dd66f959cfaa034
Galicia Negrete Macario <timgalicia@porunpaismejor.com.mx>
2026-04-16 12:32:43 -0600

    Enviroments servicios locales

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,15 @@
 export const environment = {
     production: false,
     endpoints: {
-        login: 'http://localhost:5167/api/v1/eshop/Login',
+        login: 'http://localhost:5000/api/v1/eshop/Login',
         personas: {
-            registro: 'http://localhost:5167/api/v1/eshop/Persona'
+            registro: 'http://localhost:5000/api/v1/eshop/Persona'
         },
         usuarios: {
-            registrar: 'http://localhost:5167/api/v1/eshop/Usuario',
+            registrar: 'http://localhost:5000/api/v1/eshop/Usuario',
             segundofa: {
-                generar: 'http://localhost:5167/api/v1/eshop/Usuario/segundofa',
-                activar: 'http://localhost:5167/api/v1/eshop/Usuario/segundofa/activar'
+                generar: 'http://localhost:5000/api/v1/eshop/Usuario/segundofa',
+                activar: 'http://localhost:5000/api/v1/eshop/Usuario/segundofa/activar'
             }
         }
     },
